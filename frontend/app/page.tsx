4ddd2783a73b54c2bbbc550925b3cd46be70ada9
leoocalegario <leonardo.r.calegario@hotmail.com>
2025-05-06 08:58:38 -0300

    deploy teste

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function Page() {
   const router = useRouter();
-//teste
+
   useEffect(() => {
     router.push("/home");
   }, [router]);
