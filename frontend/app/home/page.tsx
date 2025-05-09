9aa04b0f0f6b3e81a7f2078d3214391aac2b1732
leoocalegario <leonardo.r.calegario@hotmail.com>
2025-05-08 23:51:28 -0300

    teste deploy dev

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -378,7 +378,7 @@ export default function StudyRoomScheduler() {
   // Função para buscar as salas do endpoint
   const fetchRooms = useCallback(async (): Promise<Room[]> => {
     try {
-      const response = await fetch('http://34.39.140.137/api/rooms');
+      const response = await fetch('http://35.198.6.122/api/rooms');
       if (!response.ok) throw new Error("Erro ao buscar salas");
       return await response.json();
     } catch (error) {
@@ -460,7 +460,7 @@ export default function StudyRoomScheduler() {
       };
   
       // Envia para o backend com o token
-      const response = await fetch('http://34.39.140.137/api/rooms', {
+      const response = await fetch('http://35.198.6.122/api/rooms', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -496,7 +496,7 @@ export default function StudyRoomScheduler() {
         const token = localStorage.getItem("jwt");
         if (!token) throw new Error("Usuário não autenticado!");
     
-        const response = await fetch(`http://34.39.140.137/api/rooms/${roomId}`, {
+        const response = await fetch(`http://35.198.6.122/api/rooms/${roomId}`, {
           method: "DELETE",
           headers: {
             "Authorization": `Bearer ${token}`,
@@ -541,7 +541,7 @@ export default function StudyRoomScheduler() {
         end_time: formData.endTime,
       };
   
-      const response = await fetch('http://34.39.140.137/api/reservations', {
+      const response = await fetch('http://35.198.6.122/api/reservations', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(reservation),
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,20 +5,28 @@ const nextConfig = {
     return {
       beforeFiles: [
         {
+          source: '/api/rooms',
+          destination: 'http://backend.backend-dev.svc.cluster.local:8080/rooms/',
+        },
+        {
           source: '/api/rooms/:path*',
-          destination: 'http://backend.backend.svc.cluster.local:8080/rooms/:path*/',
+          destination: 'http://backend.backend-dev.svc.cluster.local:8080/rooms/:path*/',
+        },
+        {
+          source: '/api/reservations',
+          destination: 'http://backend.backend-dev.svc.cluster.local:8080/reservations/',
         },
         {
           source: '/api/reservations/:path*',
-          destination: 'http://backend.backend.svc.cluster.local:8080/reservations/:path*/',
+          destination: 'http://backend.backend-dev.svc.cluster.local:8080/reservations/:path*/',
         },
         {
           source: '/api/token',
-          destination: 'http://backend.backend.svc.cluster.local:8080/token/',
+          destination: 'http://backend.backend-dev.svc.cluster.local:8080/token/',
         },
         {
           source: '/token',
-          destination: 'http://backend.backend.svc.cluster.local:8080/token/',
+          destination: 'http://backend.backend-dev.svc.cluster.local:8080/token/',
         },
       ],
     };
