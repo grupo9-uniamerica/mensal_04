04721b5c0110268be510fdc6b3696ad65a9e6ba4
leoocalegario <leonardo.r.calegario@hotmail.com>
2025-05-30 09:45:13 -0300

    teste deployleo

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,32 @@ provider "google" {
   zone        = "southamerica-east1-a"
 }
 
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["https-server"]
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "vm-iac"
   machine_type = "e2-medium"
@@ -13,14 +39,15 @@ resource "google_compute_instance" "vm_instance" {
     initialize_params {
         image = "ubuntu-os-cloud/ubuntu-2204-lts"
         size  = 20
+    }
   }
-}
-
 
   network_interface {
     network = "default"
     access_config {}
   }
+
+  tags = ["http-server", "https-server"]
 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
