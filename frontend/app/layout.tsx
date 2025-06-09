35d6214ebdb9bcf00c82af9737c1986e8dbeaa9e
leoocalegario <leonardo.r.calegario@hotmail.com>
2025-06-09 15:34:43 -0300

    Feat: updating mensal 03

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         {children}æ
         {/* Footer */}
         <footer style={styles}>
-          Version 1.11
+          Version 1.12
         </footer>
       </body>
     </html>
