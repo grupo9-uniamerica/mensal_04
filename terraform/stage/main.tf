04a0f99c3789aa324167acc095195aa86a3aa807
leoocalegario <leonardo.r.calegario@hotmail.com>
2025-06-08 23:32:37 -0300

    Feat: Teste Staging GKE 06

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -158,7 +158,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "node-pool-prod-v2"
   cluster    = google_container_cluster.primary.name
   location   = "southamerica-east1-a"
-  node_count = 1
+  node_count = 3
 
   node_config {
     machine_type = "e2-small"
